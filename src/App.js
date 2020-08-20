09647585e1e6767bd10d2f158c5f652d16f67e24
Amalia Litsa <amalia.litsa@gmail.com>
2020-08-20 18:25:04 -0500

    dogs

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello, the cats
+          Hello, the dogs
         </p>
         <a
           className="App-link"
